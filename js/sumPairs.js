ce096057835c86b14d5306d7966297a33b811b5e
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-26 14:31:53 -0700

    struggled through it

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,34 @@
-exports.sumPairs = function() {
+exports.sumPairs = function(array, desiredSum) {
+
+    let holdingArray = []
+    let finalArray = [];
+
+    for (let i = 1; i < array.length; i++ )
+    { 
+        for (let a = i; a < array.length; a++)
+        {
+            if (array[a] == array[i]) continue; //Skips when number is the same
+            if (array[a] + array[i] == desiredSum)
+            {
+                holdingArray.push([array[i], array[a]]);
+                finalArray.push(holdingArray);
+            }
+        }
+    }
+    if (finalArray[0] === undefined)
+    {
+        return "unable to find pairs";
+    }
+    else{
+        return finalArray[0];
+    }
+        
+        
 
 };
+console.log(exports.sumPairs([1,2,3,4,5], 9));
+console.log(exports.sumPairs([1,2,3,4,5], 7));
+console.log(exports.sumPairs([3, 1, 5, 8, 2], 27));
+
+
+
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,5 @@
 var sp = require("./sumPairs");
 
-// Don't forget to add your tests :)
+console.log(exports.sumPairs([1,2,3,4,5], 9))
+console.log(exports.sumPairs([1,2,3,4,5], 7))
+console.log(exports.sumPairs([3, 1, 5, 8, 2], 27))
